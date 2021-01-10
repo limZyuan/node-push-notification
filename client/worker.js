37bc5e52e4008db887f23f7155efc6f40b73b7b6
limZyuan <58379837+limZyuan@users.noreply.github.com>
2021-01-10 22:45:55 +0800

    update on icon

--- client/worker.js
+++ client/worker.js
@@ -2,9 +2,10 @@ console.log("Service Worker Loaded...");
 
 self.addEventListener("push", (e) => {
   const data = e.data.json();
-  console.log("Push received...");
+  console.log(`Push received...`, data);
   self.registration.showNotification(data.title, {
     body: "Notified by Me!",
-    // icon: "",
+    icon:
+      "https://images.unsplash.com/photo-1533794299596-8e62c88ff975?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
   });
 });
